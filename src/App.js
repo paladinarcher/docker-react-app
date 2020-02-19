592bf9a8d30361aad7e6c04ed8fefd657ffdbf36
Carl Shurtliff <cphilshurtliff@gmail.com>
2020-02-19 15:26:44 -0700

    changed to 20 per cent

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-	  <p>Hi World</p>
+	  <p>20 per cent on car insurance</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
