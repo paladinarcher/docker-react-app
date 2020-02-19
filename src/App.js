31fad87b7ee8bfc260781cc5890bfd5e22ff0025
Carl Shurtliff <cphilshurtliff@gmail.com>
2020-02-19 15:13:20 -0700

    changed to puxatony phil

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-	  <p>Hi World</p>
+	  <p>Puxatony Phil</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
