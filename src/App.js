e7743fad83eda6e43a8cbab6b2b0eb94abfc885f
Carl Shurtliff <cphilshurtliff@gmail.com>
2020-02-19 17:33:36 -0700

    changed message to It seems to be working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-	  <p>Happy days</p>
+	  <p>It seems to be working</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
