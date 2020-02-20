e7a59c1ba4a21d44efddb5114bf35e656d888e0a
Carl Shurtliff <cphilshurtliff@gmail.com>
2020-02-20 11:43:59 -0700

    changed text to cars 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-	  <p>cars</p>
+	  <p>cars 2</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
